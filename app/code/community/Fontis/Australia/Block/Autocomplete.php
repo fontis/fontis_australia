d370b83004b041ca2cff57856a97a71540c6a4dd
Chris Norton <chris@chnorton.com.au>
2013-05-07 11:43:11 +1000

    Improvement to autocomplete query

--- app/code/community/Fontis/Australia/Block/Autocomplete.php
+++ app/code/community/Fontis/Australia/Block/Autocomplete.php
@@ -37,14 +37,18 @@ class Fontis_Australia_Block_Autocomplete extends Mage_Core_Block_Abstract
         // Get the text that the customer has entered as a query.
         $query = $this->helper('australia')->getQueryText();
         $country = $this->helper('australia')->getQueryCountry();
-        if ($country != "AU")
+        if ($country != "AU") {
             return $html;
+        }
 
+        /* @var $conn Varien_Db_Adapter_Pdo_Mysql */
         $conn = Mage::getSingleton('core/resource')->getConnection('australia_read');
         $resultArray = $conn->fetchAll(
-                'SELECT au.*, dcr.region_id FROM ' . Mage::getSingleton('core/resource')->getTableName('australia_postcode') . ' AS au
-                 INNER JOIN ' . Mage::getSingleton('core/resource')->getTableName('directory_country_region') . ' AS dcr ON au.region_code = dcr.code
-		 WHERE city LIKE \'%' . $query . '%\' ORDER BY city, region_code');
+            'SELECT au.*, dcr.region_id FROM ' . Mage::getSingleton('core/resource')->getTableName('australia_postcode') . ' AS au
+             INNER JOIN ' . Mage::getSingleton('core/resource')->getTableName('directory_country_region') . ' AS dcr ON au.region_code = dcr.code
+             WHERE city LIKE :city ORDER BY city, region_code',
+             array('city' => $query)
+        );
 
         $html = '<ul>';
         $counter = 0;
