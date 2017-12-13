3ba106ec217c362af2e7aa8cc46e960592942599
Bevan Holman <bholman@dhmedia.com.au>
2017-12-13 15:27:51 +0800

    Update consignment CSV import file format
    Closes #76

--- src/lib/Doghouse/Australia/Eparcel/Record/Article.php
+++ src/lib/Doghouse/Australia/Eparcel/Record/Article.php
@@ -1,35 +1,42 @@
-<?php 
+<?php
 
 class Doghouse_Australia_Eparcel_Record_Article extends Doghouse_Australia_Eparcel_Record
 {
-	public $type = 'A';
-	
-	/**
-	 * The cubic weight is the parcel's volume in cubic metres multiplied by 250.
-	 * @see http://auspost.com.au/personal/parcel-dimensions.html
-	 * @var float
-	 */
-	public $weight;
-	
-	public $length;
-	public $width;
-	public $height;
-	public $numberIdenticalItems = 1;
-	public $description;
-	public $isDangerousGoods = false;
-	public $isInsuranceRequired = false;
-	public $insuranceAmount;
-	public $valueForCustoms;
-	public $exportReason;
-	public $exportClearanceNumber;
-	
-	public function calculateWeight()
-	{
-		/*
-		 * Everything is already calculated.
-		 * This method only exists for consistency with 
-		 * Doghouse_Australia_Eparcel_Record_Article_CubicWeight
-		 */
-		return true;
-	}
+    public $type = 'A';
+
+    /**
+     * The cubic weight is the parcel's volume in cubic metres multiplied by 250.
+     * @see http://auspost.com.au/personal/parcel-dimensions.html
+     * @var float
+     */
+    public $weight;
+
+    public $length;
+    public $width;
+    public $height;
+    public $numberIdenticalItems = 1;
+    public $description;
+    public $isDangerousGoods = false;
+    public $isInsuranceRequired = false;
+    public $insuranceAmount;
+    public $valueForCustoms;
+    public $exportReason;
+    public $exportClearanceNumber;
+    public $isReturnSurface;
+    public $isReturnAir;
+    public $isAbandon;
+    public $isRedirectSurface;
+    public $isRedirectAir;
+    public $productClassification;
+    public $isCommercialValue;
+
+    public function calculateWeight()
+    {
+        /*
+         * Everything is already calculated.
+         * This method only exists for consistency with
+         * Doghouse_Australia_Eparcel_Record_Article_CubicWeight
+         */
+        return true;
+    }
 }
